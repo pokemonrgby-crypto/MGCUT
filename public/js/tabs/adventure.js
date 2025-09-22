49e2aaacf93eded03f6c7a9a79020e6184e19c03
Arltriaa <pokemonrgby@gmail.com>
2025-09-23 02:52:54 +0900

    Update adventure.js

--- public/js/tabs/adventure.js
+++ public/js/tabs/adventure.js
@@ -3,8 +3,8 @@ export function mount(){
   const host = document.querySelector('[data-view="adventure"] .list');
   host.innerHTML = `
     <div class="card pad">
-      <div class="t">모험 시스템</div>
-      <div class="small">다음 업데이트에서 탐험/전투 로그를 연결할게.</div>
+      <div style="font-weight:700">모험 시스템</div>
+      <div class="small">다음 업데이트에서 탐험/전투 로그를 연결할 예정입니다.</div>
     </div>
   `;
 }
