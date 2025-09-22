5cb0ac77860ae2cfef130b12b8cc0d74d7902f75
Arltriaa <pokemonrgby@gmail.com>
2025-09-22 23:51:07 +0900

    Create firebase-config.js

---
+++ public/firebase-config.js
@@ -0,0 +1,9 @@
+// Firebase 콘솔 웹앱 설정값으로 채워 넣어줘
+window.__FBCONFIG__ = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
