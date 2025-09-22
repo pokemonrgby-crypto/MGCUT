d04e1b008327b700ce8b0101abef04026a47d5ca
Arltriaa <pokemonrgby@gmail.com>
2025-09-23 02:18:30 +0900

    Create adventure.js

---
+++ public/js/tabs/adventure.js
@@ -0,0 +1,10 @@
+// /public/js/tabs/adventure.js
+export function mount(){
+  const host = document.querySelector('[data-view="adventure"] .list');
+  host.innerHTML = `
+    <div class="card pad">
+      <div class="t">모험 시스템</div>
+      <div class="small">다음 업데이트에서 탐험/전투 로그를 연결할게.</div>
+    </div>
+  `;
+}
