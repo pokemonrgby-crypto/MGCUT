559884172686a84c689890d6e4d5cee3dbb4e2c9
Arltriaa <pokemonrgby@gmail.com>
2025-09-23 13:21:57 +0900

    Update frame.js

--- public/js/ui/frame.js
+++ public/js/ui/frame.js
@@ -4,6 +4,7 @@ import * as Home from '../tabs/home.js';
 import * as Create from '../tabs/create.js';
 import * as Info from '../tabs/info.js';
 import * as Adventure from '../tabs/adventure.js';
+import * as Ranking from '../tabs/ranking.js';
 import * as CreateWorld from '../tabs/create-world.js';
 import * as CreateCharacter from '../tabs/create-character.js';
 import * as CreatePrompt from '../tabs/create-prompt.js';
@@ -11,6 +12,7 @@ import * as CreateSite from '../tabs/create-site.js';
 import * as WorldDetail from '../tabs/world-detail.js';
 import * as EpisodeDetail from '../tabs/episode-detail.js';
 import * as CharacterDetail from '../tabs/character-detail.js';
+
 
 export const ui = {
   blocker: null,
@@ -39,6 +41,7 @@ function handleRouteChange() {
     'home': { view: 'home', mount: Home.mount },
     'create': { view: 'create', mount: Create.mount },
     'adventure': { view: 'adventure', mount: Adventure.mount },
+    'ranking': { view: 'ranking', mount: Ranking.mount },
     'info': { view: 'info', mount: Info.mount },
     'create-world': { parentView: 'create', view: 'create-world' },
     'create-character': { parentView: 'create', view: 'create-character' },
@@ -47,7 +50,7 @@ function handleRouteChange() {
     'world': { parentView: 'home', view: 'world-detail', mount: () => WorldDetail.mount(param1) },
     'character': { parentView: 'home', view: 'character-detail', mount: () => CharacterDetail.mount(param1) },
     'episode': { parentView: 'home', view: 'episode-detail', mount: () => EpisodeDetail.mount(param1, decodeURIComponent(param2)) }
-};
+  };
 
   const route = routes[path];
   if (route) {
